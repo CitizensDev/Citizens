b4cdb1bfe90369565fae5b5bf97a5e3d82fbaac7
Alex Puncochar <alexpuncochar@yahoo.com>
2011-05-03 14:50:31 -0500

    a bit more healer work

--- src/com/fullwall/Citizens/Healers/HealerNPC.java
+++ src/com/fullwall/Citizens/Healers/HealerNPC.java
@@ -1,5 +1,21 @@
 package com.fullwall.Citizens.Healers;
 
-public class HealerNPC {
+import com.fullwall.resources.redecouverte.NPClib.HumanNPC;
 
+public class HealerNPC {
+	private HumanNPC npc;
+	// amount of "heal-power" remaining in the NPC, this drains as it is used
+	private int healPower;
+	
+	public HealerNPC(HumanNPC npc){
+		this.npc = npc;
+	}
+	
+	public int getHealPower(){
+		return healPower;
+	}
+	
+	public void setHealPower(int healPower){
+		this.healPower = healPower;
+	}
 }
