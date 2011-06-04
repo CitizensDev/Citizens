42448416454df746e9fb242896bac80a84de4925
Alex Puncochar <alexpuncochar@yahoo.com>
2011-06-04 04:55:54 -0500

    removed transfer code

--- src/com/fullwall/Citizens/Citizens.java
+++ src/com/fullwall/Citizens/Citizens.java
@@ -1,6 +1,5 @@
 package com.fullwall.Citizens;
 
-import java.io.File;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.List;
@@ -19,8 +18,6 @@ import com.fullwall.Citizens.NPCTypes.Guards.GuardTask;
 import com.fullwall.Citizens.NPCTypes.Healers.HealerTask;
 import com.fullwall.Citizens.NPCs.BasicNPCHandler;
 import com.fullwall.Citizens.NPCs.NPCManager;
-import com.fullwall.Citizens.Properties.ConfigurationHandler;
-import com.fullwall.Citizens.Properties.PropertyHandler;
 import com.fullwall.Citizens.Properties.PropertyManager;
 import com.fullwall.Citizens.Properties.Properties.UtilityProperties;
 import com.fullwall.Citizens.Utils.Messaging;
@@ -117,16 +114,9 @@ public class Citizens extends JavaPlugin {
 		worldListener.registerEvents();
 		serverListener.registerEvents();
 
-		Permission.initialize(getServer());
-
-		// TODO - remove on update.
-		transferSettings();
-
 		// Register files.
 		PropertyManager.registerProperties();
 
-		// TODO - remove on update.
-		transferInventories();
 		Constants.setupVariables();
 
 		plugin = this;
@@ -201,153 +191,6 @@ public class Citizens extends JavaPlugin {
 		Messaging.log("Loaded " + NPCManager.GlobalUIDs.size() + " NPCs.");
 		getServer().getScheduler().scheduleSyncRepeatingTask(this,
 				new HealerTask(), getHealthRegenRate(), getHealthRegenRate());
-	}
-
-	private void transferInventories() {
-		File file = new File("plugins/Citizens/Traders/Citizens.inventories");
-		if (file.exists()) {
-			try {
-				PropertyHandler temp = PropertyManager.getBasic().inventories;
-				PropertyHandler handler = new PropertyHandler(
-						"plugins/Citizens/Traders/Citizens.inventories");
-				for (Entry<String, String> entry : handler.returnMap()
-						.entrySet()) {
-					temp.setString(entry.getKey(), entry.getValue());
-				}
-				handler.clear();
-			} catch (Exception e) {
-			}
-			file.deleteOnExit();
-		}
-	}
-
-	private void transferSettings() {
-		File file = new File("plugins/Citizens/Citizens.settings");
-		if (file.exists()) {
-			ConfigurationHandler temp = new ConfigurationHandler(
-					"plugins/Citizens/citizens.yml", true);
-			PropertyHandler handler = new PropertyHandler(
-					"plugins/Citizens/Citizens.settings");
-			temp.setInt("general.limits.npcs-per-player",
-					handler.getInt("max-NPCs-per-player"));
-			temp.setBoolean("general.defaults.enable-following",
-					handler.getBoolean("default-enable-following"));
-			temp.setBoolean("general.defaults.talk-when-close",
-					handler.getBoolean("default-talk-when-close"));
-			temp.setBoolean("general.colors.use-npc-colours",
-					handler.getBoolean("use-npc-colours"));
-			temp.setString("general.colors.npc-colour",
-					handler.getString("npc-colour"));
-			temp.setString("general.chat.format",
-					handler.getString("chat-format"));
-			temp.setBoolean("general.chat.slashes-to-spaces",
-					handler.getBoolean("slashes-to-spaces"));
-			temp.setString("general.chat.default-text",
-					handler.getString("default-text"));
-			temp.setInt("general.wizards.wizard-max-locations",
-					handler.getInt("wizard-max-locations"));
-			// Item Settings
-			temp.setBoolean("items.item-list-on",
-					handler.getBoolean("item-list-on"));
-			temp.setString("items.basic.talk-items", handler.getString("items"));
-			temp.setString("items.basic.select-items",
-					handler.getString("select-item"));
-			temp.setInt("items.healers.take-health-item",
-					handler.getInt("healer-take-health-item"));
-			temp.setInt("items.healers.give-health-item",
-					handler.getInt("healer-give-health-item"));
-			temp.setInt("items.wizards.interact-item",
-					handler.getInt("wizard-interact-item"));
-			// Tick Settings
-			temp.setInt("ticks.general.delay", handler.getInt("tick-delay"));
-			temp.setBoolean("ticks.saving.use-task",
-					handler.getBoolean("use-save-task"));
-			temp.setBoolean("ticks.saving.save-often",
-					handler.getBoolean("save-often"));
-			temp.setInt("ticks.saving.delay", handler.getInt("save-tick-delay"));
-			temp.setInt("ticks.healers.health-regen-increment",
-					handler.getInt("healer-health-regen-increment"));
-			// Range Settings
-			temp.setInt("range.basic.look", handler.getInt("look-range"));
-			handler.clear();
-			file.deleteOnExit();
-		}
-		file = new File("plugins/Citizens/Citizens.economy");
-		if (file.exists()) {
-			ConfigurationHandler temp = new ConfigurationHandler(
-					"plugins/Citizens/economy.yml", true);
-			PropertyHandler handler = new PropertyHandler(
-					"plugins/Citizens/Citizens.economy");
-			temp.setBoolean("economy.use-economy",
-					handler.getBoolean("use-economy"));
-			temp.setBoolean("economy.use-econplugin",
-					handler.getBoolean("use-econplugin"));
-
-			temp.setInt("prices.basic.creation.item",
-					handler.getInt("basic-npc-create-item"));
-			temp.setInt("prices.basic.creation.item-currency-id",
-					handler.getInt("basic-npc-create-item-currency-id"));
-			temp.setInt("prices.basic.creation.econplugin",
-					handler.getInt("basic-npc-create-econplugin"));
-
-			temp.setInt("prices.blacksmith.creation.item",
-					handler.getInt("blacksmith-npc-create-item"));
-			temp.setInt("prices.blacksmith.creation.item-currency-id",
-					handler.getInt("blacksmith-npc-create-item-currency-id"));
-			temp.setInt("prices.blacksmith.creation.econplugin",
-					handler.getInt("blacksmith-npc-create-econplugin"));
-
-			temp.setInt("prices.blacksmith.armorrepair.item",
-					handler.getInt("blacksmith-armor-repair-item"));
-			temp.setInt("prices.blacksmith.armorrepair.item-currency-id",
-					handler.getInt("blacksmith-armor-repair-item-currency-id"));
-			temp.setInt("prices.blacksmith.armorrepair.econplugin",
-					handler.getInt("blacksmith-armor-repair-econplugin"));
-
-			temp.setInt("prices.blacksmith.toolrepair.item",
-					handler.getInt("blacksmith-tool-repair-item"));
-			temp.setInt("prices.blacksmith.toolrepair.item-currency-id",
-					handler.getInt("blacksmith-tool-repair-item-currency-id"));
-			temp.setInt("prices.blacksmith.toolrepair.econplugin",
-					handler.getInt("blacksmith-tool-repair-econplugin"));
-
-			temp.setInt("prices.healer.creation.item",
-					handler.getInt("healer-npc-create-item"));
-			temp.setInt("prices.healer.creation.item-currency-id",
-					handler.getInt("healer-npc-create-item-currency-id"));
-			temp.setInt("prices.healer.creation.econplugin",
-					handler.getInt("healer-npc-create-econplugin"));
-
-			temp.setInt("prices.healer.levelup.item",
-					handler.getInt("healer-level-up-item"));
-			temp.setInt("prices.healer.levelup.item-currency-id",
-					handler.getInt("healer-level-up-item-currency-id"));
-			temp.setInt("prices.healer.levelup.econplugin",
-					handler.getInt("healer-level-up-econplugin"));
-
-			temp.setInt("prices.trader.creation.item",
-					handler.getInt("trader-npc-create-item"));
-			temp.setInt("prices.trader.creation.item-currency-id",
-					handler.getInt("trader-npc-create-item-currency-id"));
-			temp.setInt("prices.trader.creation.econplugin",
-					handler.getInt("trader-npc-create-econplugin"));
-
-			temp.setInt("prices.wizard.creation.item",
-					handler.getInt("wizard-npc-create-item"));
-			temp.setInt("prices.wizard.creation.item-currency-id",
-					handler.getInt("wizard-npc-create-item-currency-id"));
-			temp.setInt("prices.wizard.creation.econplugin",
-					handler.getInt("wizard-npc-create-econplugin"));
-
-			temp.setInt("prices.wizard.teleport.item",
-					handler.getInt("wizard-teleport-item"));
-			temp.setInt("prices.wizard.teleport.item-currency-id",
-					handler.getInt("wizard-teleport-item-currency-id"));
-			temp.setInt("prices.wizard.teleport.econplugin",
-					handler.getInt("wizard-teleport-econplugin"));
-			handler.clear();
-			file.deleteOnExit();
-		}
 	}
 
 	/**
