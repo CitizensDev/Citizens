c4ff6651cbbdf0be5467885ffdf63f25d549d6f2
fullwall <fullwall@optusnet.com>
2011-09-30 20:18:34 +0800

    Make anti-exploits more general

--- src/quester/net/citizensnpcs/questers/listeners/QuesterBlockListen.java
+++ src/quester/net/citizensnpcs/questers/listeners/QuesterBlockListen.java
@@ -1,6 +1,6 @@
 package net.citizensnpcs.questers.listeners;
 
-import java.util.Map;
+import java.util.Set;
 
 import net.citizensnpcs.Citizens;
 import net.citizensnpcs.SettingsManager;
@@ -9,20 +9,18 @@ import net.citizensnpcs.questers.QuestManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
-import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 public class QuesterBlockListen extends BlockListener {
-	private static final Map<Location, Player> placed = Maps.newHashMap();
+	private static final Set<Location> placed = Sets.newHashSet();
 
 	@Override
 	public void onBlockBreak(BlockBreakEvent event) {
-		Player placer = placed.get(event.getBlock().getLocation());
-		if (placer != null && placer.equals(event.getPlayer())) {
+		if (placed.contains(event.getBlock().getLocation())) {
 			return;
 		}
 		QuestManager.incrementQuest(event.getPlayer(), event);
@@ -30,7 +28,7 @@ public class QuesterBlockListen extends BlockListener {
 
 	@Override
 	public void onBlockPlace(BlockPlaceEvent event) {
-		placed.put(event.getBlock().getLocation(), event.getPlayer());
+		placed.add(event.getBlock().getLocation());
 		Bukkit.getScheduler().scheduleSyncDelayedTask(Citizens.plugin,
 				new RemoveBlockTask(event.getBlock()),
 				SettingsManager.getInt("BlockTrackingRemoveDelay"));
--- src/quester/net/citizensnpcs/questers/quests/types/CollectQuest.java
+++ src/quester/net/citizensnpcs/questers/quests/types/CollectQuest.java
@@ -1,6 +1,6 @@
 package net.citizensnpcs.questers.quests.types;
 
-import java.util.Map;
+import java.util.Set;
 
 import net.citizensnpcs.Citizens;
 import net.citizensnpcs.SettingsManager;
@@ -11,29 +11,27 @@ import net.citizensnpcs.utils.StringUtils;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Type;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 
-import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 public class CollectQuest implements QuestUpdater {
 	private static final Type[] EVENTS = { Type.PLAYER_DROP_ITEM,
 			Type.PLAYER_PICKUP_ITEM };
-	private static final Map<Item, Player> dropped = Maps.newHashMap();
+	private static final Set<Item> dropped = Sets.newHashSet();
 
 	@Override
 	public boolean update(Event event, ObjectiveProgress progress) {
 		if (event instanceof PlayerDropItemEvent) {
 			PlayerDropItemEvent ev = (PlayerDropItemEvent) event;
-			dropped.put(ev.getItemDrop(), ev.getPlayer());
+			dropped.add(ev.getItemDrop());
 			new ItemCheck(ev.getItemDrop()).run();
 		} else if (event instanceof PlayerPickupItemEvent) {
 			PlayerPickupItemEvent ev = (PlayerPickupItemEvent) event;
-			Player thrower = dropped.get(ev.getItem());
-			if (thrower != null && thrower.equals(ev.getPlayer())) {
+			if (dropped.contains(ev.getItem())) {
 				return progress.getAmount() >= progress.getObjective()
 						.getAmount();
 			}
