880e63ce6d7eab5e65e16ed5b45ee7bbbbfb14e6
fullwall <fullwall@optusnet.com>
2011-10-06 22:54:56 +0800

    Workaround to enhance RB #1240 compatibility

--- src/core/net/citizensnpcs/resources/npclib/NPCSpawner.java
+++ src/core/net/citizensnpcs/resources/npclib/NPCSpawner.java
@@ -1,9 +1,13 @@
 package net.citizensnpcs.resources.npclib;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
 import net.citizensnpcs.api.event.npc.NPCRemoveEvent;
 import net.citizensnpcs.api.event.npc.NPCRemoveEvent.NPCRemoveReason;
 import net.citizensnpcs.properties.properties.UtilityProperties;
 import net.citizensnpcs.resources.npclib.creatures.CreatureNPCType;
+import net.citizensnpcs.utils.Messaging;
 import net.citizensnpcs.utils.PacketUtils;
 import net.minecraft.server.ItemInWorldManager;
 import net.minecraft.server.MinecraftServer;
@@ -16,8 +20,27 @@ import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.entity.CraftEntity;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
 
 public class NPCSpawner {
+	private static Map<String, CraftPlayer> playerMap = getPlayerMap();
+
+	@SuppressWarnings("unchecked")
+	private static Map<String, CraftPlayer> getPlayerMap() {
+		Field f;
+		try {
+			f = CraftEntity.class.getDeclaredField("players");
+			f.setAccessible(true);
+			return (Map<String, CraftPlayer>) f.get(null);
+		} catch (Exception ex) {
+			Messaging.log("Unable to fetch player map from CraftEntity: "
+					+ ex.getMessage()
+					+ ". Conflicting names will do funny things.");
+		}
+		return null;
+	}
+
 	private static WorldServer getWorldServer(World world) {
 		return ((CraftWorld) world).getHandle();
 	}
@@ -26,7 +49,13 @@ public class NPCSpawner {
 		return ((CraftServer) server).getServer();
 	}
 
+	private static void clearMap(String name) {
+		if (playerMap != null)
+			playerMap.remove(name);
+	}
+
 	public static HumanNPC spawnNPC(int UID, String name, Location loc) {
+		clearMap(name);
 		WorldServer ws = getWorldServer(loc.getWorld());
 		CraftNPC eh = new CraftNPC(getMinecraftServer(ws.getServer()), ws,
 				name, new ItemInWorldManager(ws));
@@ -34,12 +63,14 @@ public class NPCSpawner {
 				loc.getYaw(), loc.getPitch());
 		ws.addEntity(eh);
 		ws.players.remove(eh);
+		clearMap(name);
 		return new HumanNPC(eh, UID, name);
 	}
 
 	public static HumanNPC spawnNPC(Location loc, CreatureNPCType type) {
 		try {
 			String name = UtilityProperties.getRandomName(type);
+			clearMap(name);
 			WorldServer ws = getWorldServer(loc.getWorld());
 			CraftNPC eh = type.getEntityConstructor().newInstance(
 					getMinecraftServer(ws.getServer()), ws, name,
@@ -48,6 +79,7 @@ public class NPCSpawner {
 					loc.getYaw(), loc.getPitch());
 			ws.addEntity(eh);
 			ws.players.remove(eh);
+			clearMap(name);
 			return new HumanNPC(eh, -1 /*Fake UID*/, name);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -56,11 +88,13 @@ public class NPCSpawner {
 	}
 
 	public static HumanNPC spawnNPC(HumanNPC npc, Location loc) {
+		clearMap(npc.getName());
 		WorldServer ws = getWorldServer(loc.getWorld());
 		npc.getHandle().setPositionRotation(loc.getX(), loc.getY(), loc.getZ(),
 				loc.getYaw(), loc.getPitch());
 		ws.addEntity(npc.getHandle());
 		ws.players.remove(npc.getHandle());
+		clearMap(npc.getName());
 		return npc;
 	}
 
