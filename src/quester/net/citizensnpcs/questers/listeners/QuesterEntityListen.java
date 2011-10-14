743244d80fd1e73aca5fd16ae7b087a660360e4e
fullwall <fullwall@optusnet.com>
2011-10-14 13:15:19 +0800

    Support projectile deaths in hunt quests

--- src/quester/net/citizensnpcs/questers/listeners/QuesterEntityListen.java
+++ src/quester/net/citizensnpcs/questers/listeners/QuesterEntityListen.java
@@ -4,6 +4,7 @@ import net.citizensnpcs.questers.QuestManager;
 
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityListener;
@@ -14,6 +15,14 @@ public class QuesterEntityListen extends EntityListener {
 		if (ev.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent) {
 			EntityDamageByEntityEvent event = (EntityDamageByEntityEvent) ev
 					.getEntity().getLastDamageCause();
+			if (event.getDamager() instanceof Projectile) {
+				Projectile shot = ((Projectile) event.getDamager());
+				if (shot.getShooter() instanceof Player) {
+					event = new EntityDamageByEntityEvent(shot.getShooter(),
+							event.getEntity(), event.getCause(),
+							event.getDamage());
+				}
+			}
 			if (event.getDamager() instanceof Player
 					&& event.getEntity() instanceof LivingEntity) {
 				QuestManager.incrementQuest((Player) event.getDamager(), ev);
