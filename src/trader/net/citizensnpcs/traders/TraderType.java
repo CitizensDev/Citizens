20ac35a51fb7c24684979025d3217716af5b5a4c
fullwall <fullwall@optusnet.com>
2011-10-28 23:23:55 +0800

    Fix trader global prices not loading

--- src/trader/net/citizensnpcs/traders/TraderType.java
+++ src/trader/net/citizensnpcs/traders/TraderType.java
@@ -3,7 +3,10 @@ package net.citizensnpcs.traders;
 import net.citizensnpcs.commands.CommandHandler;
 import net.citizensnpcs.npctypes.CitizensNPC;
 import net.citizensnpcs.npctypes.CitizensNPCType;
+import net.citizensnpcs.npctypes.NPCTypeManager;
 import net.citizensnpcs.properties.Properties;
+
+import org.bukkit.event.Event.Type;
 
 public class TraderType extends CitizensNPCType {
 	@Override
@@ -19,6 +22,11 @@ public class TraderType extends CitizensNPCType {
 	@Override
 	public String getName() {
 		return "trader";
+	}
+
+	@Override
+	public void registerEvents() {
+		NPCTypeManager.registerEvent(Type.CUSTOM_EVENT, new CitizensListen());
 	}
 
 	@Override
