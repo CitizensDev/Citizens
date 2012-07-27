184ba3bad57e59906be81c59fd996ad80b7cf92a
fullwall <fullwall@optusnet.com>
2012-07-27 19:03:24 +0800

    Rework action clearing

--- src/core/net/citizensnpcs/TickTask.java
+++ src/core/net/citizensnpcs/TickTask.java
@@ -1,5 +1,7 @@
 package net.citizensnpcs;
 
+import java.util.List;
+
 import net.citizensnpcs.api.event.NPCCreateEvent.NPCCreateReason;
 import net.citizensnpcs.resources.npclib.HumanNPC;
 import net.citizensnpcs.resources.npclib.NPCManager;
@@ -15,14 +17,22 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.SetMultimap;
 
 public class TickTask implements Runnable {
     private static final SetMultimap<HumanNPC, String> cachedActions = HashMultimap.create();
+    private static final List<String> toRemove = Lists.newArrayList();
 
     @Override
     public void run() {
         Player[] online = Bukkit.getServer().getOnlinePlayers();
+        if (toRemove.size() > 0) {
+            synchronized (toRemove) {
+                cachedActions.values().removeAll(toRemove);
+                toRemove.clear();
+            }
+        }
         for (HumanNPC npc : NPCManager.getList().values()) {
             updateWaypoints(npc);
             npc.doTick();
@@ -51,46 +61,47 @@ public class TickTask implements Runnable {
     private void updateWaypoints(HumanNPC npc) {
         WaypointPath waypoints = npc.getWaypoints();
         switch (waypoints.size()) {
-        case 0:
-            break;
-        case 1:
-            // TODO: merge the default and this case.
-            if (waypoints.currentIndex() >= 1) {
+            case 0:
+                break;
+            case 1:
+                // TODO: merge the default and this case.
+                if (waypoints.currentIndex() >= 1) {
+                    if (!waypoints.isStarted()) {
+                        waypoints.schedule(npc, 0);
+                    }
+                    if (waypoints.isStarted() && !npc.isPaused() && npc.getHandle().pathFinished()) {
+                        waypoints.setIndex(0);
+                    }
+                } else {
+                    if (!npc.getWaypoints().isStarted()) {
+                        PathUtils.createPath(npc, npc.getNPCData().getLocation(), -1, -1,
+                                Settings.getDouble("PathfindingRange"));
+                        waypoints.setStarted(true);
+                    }
+                    if (waypoints.isStarted() && !npc.isPaused() && npc.getHandle().pathFinished()) {
+                        waypoints.setIndex(1);
+                    }
+                }
+                if (waypoints.isStarted() && !npc.isPaused() && npc.getHandle().pathFinished()) {
+                    waypoints.setStarted(false);
+                    waypoints.onReach(npc);
+                }
+                break;
+            default:
                 if (!waypoints.isStarted()) {
-                    waypoints.schedule(npc, 0);
+                    waypoints.scheduleNext(npc);
                 }
                 if (waypoints.isStarted() && !npc.isPaused() && npc.getHandle().pathFinished()) {
-                    waypoints.setIndex(0);
+                    waypoints.setIndex(waypoints.currentIndex() + 1);
+                    waypoints.setStarted(false);
+                    waypoints.onReach(npc);
                 }
-            } else {
-                if (!npc.getWaypoints().isStarted()) {
-                    PathUtils.createPath(npc, npc.getNPCData().getLocation(), -1, -1,
-                            Settings.getDouble("PathfindingRange"));
-                    waypoints.setStarted(true);
-                }
-                if (waypoints.isStarted() && !npc.isPaused() && npc.getHandle().pathFinished()) {
-                    waypoints.setIndex(1);
-                }
-            }
-            if (waypoints.isStarted() && !npc.isPaused() && npc.getHandle().pathFinished()) {
-                waypoints.setStarted(false);
-                waypoints.onReach(npc);
-            }
-            break;
-        default:
-            if (!waypoints.isStarted()) {
-                waypoints.scheduleNext(npc);
-            }
-            if (waypoints.isStarted() && !npc.isPaused() && npc.getHandle().pathFinished()) {
-                waypoints.setIndex(waypoints.currentIndex() + 1);
-                waypoints.setStarted(false);
-                waypoints.onReach(npc);
-            }
         }
     }
 
     private static void cacheActions(HumanNPC npc, Player player) {
-        if (!cachedActions.containsEntry(npc, player.getName().toLowerCase()) && npc.getNPCData().isTalkClose()) {
+        if (!cachedActions.containsEntry(npc, player.getName().toLowerCase())
+                && npc.getNPCData().isTalkClose()) {
             MessageUtils.sendText(npc, player);
             cachedActions.put(npc, player.getName().toLowerCase());
         }
@@ -116,13 +127,14 @@ public class TickTask implements Runnable {
         @Override
         public void run() {
             NPCManager.register(UID, owner, NPCCreateReason.RESPAWN);
-            Messaging.sendUncertain(owner, StringUtils.wrap(NPCManager.get(UID).getName()) + " has respawned.");
+            Messaging.sendUncertain(owner, StringUtils.wrap(NPCManager.get(UID).getName())
+                    + " has respawned.");
         }
     }
 
     public static void clearActions(Player player) {
-        for (HumanNPC npc : cachedActions.keySet()) {
-            cachedActions.remove(npc, player.getName().toLowerCase());
+        synchronized (toRemove) {
+            toRemove.add(player.getName().toLowerCase());
         }
     }
 }
